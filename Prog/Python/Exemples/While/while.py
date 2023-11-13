f67235a7e0f8d213191e3aa3e2446e38956f352b
elRohit <r.kumar@sapalomera.cat>
2023-11-13 16:36:37 +0100

    Create while.py
    Exemples While en py

---
+++ Prog/Python/Exemples/While/while.py
@@ -0,0 +1,33 @@
+# Bucle Infinito
+'''
+while 5 > 3:
+    print("Hola Mundo!")
+'''
+# Repetir el bucle fins que posi la paraula correcta
+'''
+s= input("Paraula per sortir: ")
+while s != "sortir":
+    s= input("Paraula per sortir: ")
+'''
+
+# Menú amb While
+'''
+resposta= ""
+while resposta != "4":
+    print("1. Saluda")
+    print("2. Insulta")
+    print("3. Halagar")
+    print("4. Sortir")
+    resposta = input("Indrodueix una opció: ")  
+    if resposta == "1":
+        print("Hola!")
+    elif resposta == "2":
+        print("Ets un idiota!")
+    elif resposta == "3":
+        print("Que bé ho fas!")
+    elif resposta == "4":
+        print("Adéu!")
+    elif resposta != "4":
+        print("Apren a llegir ciego!")
+'''         
+
